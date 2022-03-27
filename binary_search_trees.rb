13c731478d12956c5f5941700b4b1c97a24ffae8
Joshua <joshuagpatten@gmail.com>
2022-03-27 18:18:40 -0400

    complete rest of project, completes tests successfully

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -8,15 +8,9 @@ class Node
     @l_child = nil
     @r_child = nil
   end
-
-  def isLeaf?
-    @l_child == nil && @r_child == nil ? true : false  
-  end
-
 end
 
 class Tree
-
   attr_accessor :root
 
   def initialize
@@ -42,8 +36,6 @@ class Tree
     else return find(root.l_child, data)
     end
   end
-
-
 
   def insert(value)
     current_node = @root
@@ -106,7 +98,107 @@ class Tree
     end
     return minv
   end
-  
+
+  def level_order(&block)
+    queue = [@root]
+    output = []
+    while !queue.empty?
+      queue.push(queue[0].l_child) if queue[0].l_child
+      queue.push(queue[0].r_child) if queue[0].r_child
+      if block_given?
+        yield queue[0].data
+      end
+      unless block_given?
+        output.push(queue[0].data)
+        queue.shift  
+      end
+    end
+    p output
+  end
+
+  def preorder(current_node, output = nil, &block)
+    output = [] if output.nil?
+    return if current_node.nil?
+
+    output.push(current_node.data) unless block_given?
+    block.call(current_node.data) if block_given?
+    preorder(current_node.l_child, output)
+    preorder(current_node.r_child, output)
+    output
+  end
+
+  def inorder(current_node, output = nil, &block)
+    output = [] if output.nil?
+    return if current_node.nil?
+
+    inorder(current_node.l_child, output)
+    output.push(current_node.data) unless block_given?
+    block.call(current_node.data) if block_given?
+    inorder(current_node.r_child, output)
+    output
+  end
+
+  def postorder(current_node, output = nil, &block)
+    output = [] if output.nil?
+    return if current_node.nil?
+
+    postorder(current_node.l_child, output)
+    postorder(current_node.r_child, output)
+    output.push(current_node.data) unless block_given?
+    block.call(current_node.data) if block_given?
+    output
+  end 
+
+  def height(root)
+    return 0 if root.nil?
+
+    left = height(root.l_child)
+    right = height(root.r_child)
+    height = (left < right) ? (right + 1) : (left + 1)
+    return height
+  end
+
+  def depth(data, depth = nil, root = nil)
+    root = @root if root.nil?
+    depth = 0 if depth.nil?
+    return 'doesn\'t exist in tree' if find(@root, data).nil?
+    return depth if root == nil || root.data == data
+
+    if root.data < data
+      depth += 1
+      return depth(data, depth, root.r_child)
+    elsif root.data > data
+      depth += 1
+      return depth(data, depth, root.l_child)
+    end
+    return depth
+  end
+
+  def balanced?(root, balanced = nil)
+    balanced = [] if balanced.nil?
+    return if !balanced
+    return if root.nil?
+    balanced?(root.l_child, balanced)
+    balanced?(root.r_child, balanced)
+    left = height(root.l_child)
+    # p 'data:'
+    # p root.data
+    # p 'left:'
+    # p left
+    right = height(root.r_child)
+    # p 'right:'
+    # p right
+    if left - right > 1 || right - left > 1
+      balanced.push(false)
+    end
+    (balanced.include? false) ? false : true
+  end
+
+  def rebalance
+    arr = inorder(@root)
+    @root = build_tree(arr, 0, arr.length - 1)
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.r_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.r_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -143,35 +235,39 @@ def mergesort(array)
   end
 end
 
-arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 0]
+arr = (Array.new(15) { rand(1..100) })
 arr = mergesort(arr).uniq
 tree = Tree.new
-tree.insert(9)
 tree.root = tree.build_tree(arr, 0, arr.length - 1)
 tree.pretty_print
-puts""
- tree.insert(9)
- tree.insert(20)
- tree.insert(1)
- tree.insert(-1)
- tree.pretty_print
- puts ""
- tree.delete(4)
- tree.pretty_print
- puts ""
- tree.delete(2)
- tree.pretty_print
- puts ""
- tree.delete(0)
- tree.pretty_print
- puts ""
- tree.delete(8)
- tree.pretty_print
- puts ""
-
- tree.delete(5)
- tree.pretty_print
- puts ""
-p tree.find(tree.root, -1)
+puts 'Is tree balanced?'
+p tree.balanced?(tree.root)
+puts 'Breadth first traversal:'
+tree.level_order
+puts 'Preorder traversal:'
+p tree.preorder(tree.root)
+puts 'Postorder traversal:'
+p tree.postorder(tree.root)
+puts 'Inorder traversal:'
+p tree.inorder(tree.root)
+for i in 1..15 do
+  tree.insert(rand(101..200))
+end
+tree.pretty_print
+puts 'Is tree balanced?'
+p tree.balanced?(tree.root)
+puts ""
+tree.rebalance
+tree.pretty_print
+puts 'Is tree balanced?'
+p tree.balanced?(tree.root)
+puts 'Breadth first traversal:'
+tree.level_order
+puts 'Preorder traversal:'
+p tree.preorder(tree.root)
+puts 'Postorder traversal:'
+p tree.postorder(tree.root)
+puts 'Inorder traversal:'
+p tree.inorder(tree.root)
 
 
